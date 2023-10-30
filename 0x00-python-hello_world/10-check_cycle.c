04de3334d56ef09bec79cb26f3fc4f60f94bc9b5
simobls <belassalsimo78@gmail.com>
2023-10-30 20:32:30 +0100

    add

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+int check_cycle(listint_t *head) {
+		listint_t* tortoise;
+		listint_t* hare;
+		hare = head;
+		tortoise = head;
+    if (head == NULL || head->next == NULL) {
+        return (0);
+    }
+
+    while (hare != NULL && hare->next != NULL) {
+        tortoise = tortoise->next;
+        hare = hare->next->next;
+
+        if (tortoise == hare) {
+            return (1);
+        }
+    }
+
+    return (0);
+}
