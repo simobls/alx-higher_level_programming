bb290e4710ef42f2d69847151d8490b1051cc73d
simobls <belassalsimo78@gmail.com>
2023-10-31 14:03:06 +0100

    add

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-for i in range(0, 10):
+for i in range(0, 8):
     for j in range(i, 10):
         if i != j:
             print("{:d}{:d}".format(i, j), end=', ')
+print("{:d}{:d}".format(i+1, j))
