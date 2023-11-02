1281d9e063c0d48eac8a4348fd823f3f6b887385
simobls <belassalsimo78@gmail.com>
2023-11-02 13:57:07 +0100

    add

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import calculator_1
+
+a = 10
+b = 5
+
+print(calculator_1.add(a, b))
+print(calculator_1.sub(a, b))
+print(calculator_1.mul(a, b))
+print(calculator_1.div(a, b))
