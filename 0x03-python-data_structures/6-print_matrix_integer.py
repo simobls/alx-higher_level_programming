f71b05a50d760e49c58e953cc55b050fc9cadba4
simobls <belassalsimo78@gmail.com>
2023-11-05 10:47:29 +0100

    add

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for i in matrix:
-            print(("{:d}".format(j) for j in i), end=' ')
+            print(" ".join("{:d}".format(j) for j in i))
