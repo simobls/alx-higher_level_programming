986d70d9432b6efda8da4a53486bf3660e5f16b1
simobls <belassalsimo78@gmail.com>
2023-11-08 13:15:54 +0100

    add

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    
+    new_matrix = []
+    for row in matrix:
+        result = list(map(lambda x: x**2, row))
+        new_matrix.append(result)
+    return new_matrix
