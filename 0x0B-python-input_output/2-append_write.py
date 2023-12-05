67f2ebe1bca8a8e2a816637fdac5b448fb4a8ad6
simobls <belassalsimo78@gmail.com>
2023-12-05 19:27:39 +0100

    add

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -4,7 +4,7 @@ Contains the append_file function
 """
 
 
-def append_file(filename="", text=""):
+def append_write(filename="", text=""):
     """""append a text into a file(UTF8)"""
     with open(filename, "a", encoding="utf-8") as f:
         return f.write(text)
