0041bd177d82509c45dd3865a3e2d8c1f5674009
simobls <belassalsimo78@gmail.com>
2024-02-16 23:37:51 +0100

    add

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Lists states"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                           passwd=argv[2], db=argv[3], charset="utf8")
+    cur = conn.cursor()
+    query = "SELECT c.id, c.name, s.name " + \
+            "FROM cities as c INNER JOIN states as s " + \
+            "ON s.id = c.state_id ORDER BY c.id ASC"
+    cur.execute(query)
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
