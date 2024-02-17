77ef11ba1d12cea8a8a87189dc6ca5bf3a6c51b5
simobls <belassalsimo78@gmail.com>
2024-02-17 15:27:53 +0100

    add

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""List all states that contains the letter a"""
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'
+        .format(argv[1], argv[2],
+                argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    states = session.query(State).filter(State.name == argv[4]).\
+        order_by(State.id).all()
+    for state in states:
+        print("{}".format(state.id))
+    session.close()
+    session.close()
