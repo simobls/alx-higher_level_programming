c63d40f4b81ee93018c940701fa0e4fc85edf7a9
simobls <belassalsimo78@gmail.com>
2024-02-18 11:51:18 +0100

    add

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -14,4 +14,3 @@ class State(Base):
     id = Column(Integer, primary_key=True, nullable=False,
                 autoincrement=True, unique=True)
     name = Column(String(128), nullable=False)
-    cities = relationship("City", backref="state")
