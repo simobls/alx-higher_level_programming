98f0264e0edf9b70aebb3119cd25938cd57cfcfa
simobls <belassalsimo78@gmail.com>
2024-02-18 14:00:02 +0100

    add

--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -2,8 +2,7 @@
 """Lists states"""
 
 from sqlalchemy import Column, Integer, String, ForeignKey
-from model_state import Base
-
+from relationship_state import Base
 
 class City(Base):
     """Class representing the states table"""
--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -4,7 +4,8 @@
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship, backref
-from model_state import Base
+
+Base = declarative_base()
 
 
 class State(Base):
