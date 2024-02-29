cf843190003707cdc0a6d5863c132ced8be44e6d
simobls <belassalsimo78@gmail.com>
2024-02-29 14:58:35 +0100

    add

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,21 +1,3 @@
 #!/bin/bash
-
-# Check if URL argument is provided
-if [ $# -eq 0 ]; then
-    echo "Usage: $0 <URL>"
-    exit 1
-fi
-
-# Get the URL from command line argument
-url=$1
-
-# Use curl to send a request and get the size of the response body
-size=$(curl -sI "$url" | grep -i content-length | awk '{print $2}' | tr -d '\r')
-
-# Check if size is obtained successfully
-if [ -z "$size" ]; then
-    echo "Error: Unable to get response size for $url"
-    exit 1
-fi
-
-echo "$size"
+# Get the byte size of the HTTP response header for a given URL.
+curl -s "$1" | wc -c
