bf550d8235dbbff4456c87ce252f8a109b0429c2
simobls <belassalsimo78@gmail.com>
2024-03-02 19:08:12 +0100

    add

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-
+"""Fetches https://alx-intranet.hbtn.io/status."""
 import requests
 
+
 if __name__ == "__main__":
-    url = 'https://intranet.hbtn.io/status'
-    response = requests.get(url)
+    r = requests.get("https://alx-intranet.hbtn.io/status")
     print("Body response:")
-    print(f"\t- type: {type(response.text)}")
-    print(f"\t- content: {response.text}")
+    print("\t- type: {}".format(type(r.text)))
+    print("\t- content: {}".format(r.text))
